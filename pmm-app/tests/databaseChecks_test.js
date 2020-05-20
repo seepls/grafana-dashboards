98b6e2d28800041d3db02d1b61034b52c31698df
Puneet Kala <puneet.0191@gmail.com>
2020-05-20 14:47:44 +0530

    SAAS-91 Another check try

--- pmm-app/tests/databaseChecks_test.js
+++ pmm-app/tests/databaseChecks_test.js
@@ -3,12 +3,12 @@ Feature('Database Failed Checks (disabledSTT state)');
 Before( async (I, pmmSettingsPage) => {
     I.Authorize();
     I.amOnPage(pmmSettingsPage.url);
-    await pmmSettingsPage.disableSTT();
 });
 
 Scenario(
     'PMM-T294 Verify user is able to see message about Disabled STT in Checks panel at Home Page @not-pr-pipeline',
-    async (I, homePage, databaseChecksPage) => {
+    async (I, homePage, databaseChecksPage, pmmSettingsPage) => {
+        await pmmSettingsPage.disableSTT();
         I.amOnPage(homePage.url);
         I.waitForVisible(homePage.fields.sttDisabledFailedChecksPanelSelector, 30);
         I.see(databaseChecksPage.messages.homePagePanelMessage, homePage.fields.sttDisabledFailedChecksPanelSelector);
@@ -17,7 +17,8 @@ Scenario(
 
 Scenario(
     'PMM-T295 Verify user is able to see message about Disabled STT at Database Checks page @not-pr-pipeline',
-    async (I, databaseChecksPage) => {
+    async (I, databaseChecksPage, pmmSettingsPage) => {
+        await pmmSettingsPage.disableSTT();
         I.amOnPage(databaseChecksPage.url);
         I.waitForVisible(databaseChecksPage.fields.dbCheckPanelSelector, 30);
         I.waitForVisible(databaseChecksPage.fields.disabledSTTMessageSelector, 30);
@@ -26,53 +27,49 @@ Scenario(
     }
 );
 
-// Feature('Database Failed Checks (enabledSTT state)');
+Scenario(
+    'PMM-T233 PMM-T234 Verify user is able to access PMM Database Checks through UI and with URL @not-pr-pipeline',
+    async (I, adminPage, databaseChecksPage, pmmSettingsPage) => {
+        await pmmSettingsPage.enableSTT(); 
+        adminPage.selectItemFromPMMDropdown('PMM Database Checks');
+        I.waitForVisible(databaseChecksPage.fields.dbCheckPanelSelector, 30);
+        await databaseChecksPage.verifyDatabaseChecksPageOpened();
+        I.amOnPage(databaseChecksPage.url);
+        I.waitForVisible(databaseChecksPage.fields.dbCheckPanelSelector, 30);
+        await databaseChecksPage.verifyDatabaseChecksPageOpened();
+    }
+);
 
-// Before( async (I, pmmSettingsPage) => {
-//     I.Authorize();
-//     I.amOnPage(pmmSettingsPage.url);
-//     await pmmSettingsPage.enableSTT();
-// });
+Scenario(
+    'PMM-T233 Verify user can see Number of failed checks at Home Page and open PMM Database Checks page from it @not-pr-pipeline',
+    async (I, homePage, databaseChecksPage, pmmSettingsPage) => {
+        await pmmSettingsPage.enableSTT();
+        I.amOnPage(homePage.url);
+        I.waitForVisible(homePage.fields.checksPanelSelector, 30);
+        await homePage.waitForCheckResultsToAppearInPanel();
+        I.doubleClick(homePage.fields.sttFailedChecksPanelSelector);
+        await databaseChecksPage.verifyDatabaseChecksPageOpened();
+    }
+);
 
-// Scenario(
-//     'PMM-T233 PMM-T234 Verify user is able to access PMM Database Checks through UI and with URL @not-pr-pipeline',
-//     async (I, adminPage, databaseChecksPage) => {
-//         adminPage.selectItemFromPMMDropdown('PMM Database Checks');
-//         I.waitForVisible(databaseChecksPage.fields.dbCheckPanelSelector, 30);
-//         await databaseChecksPage.verifyDatabaseChecksPageOpened();
-//         I.amOnPage(databaseChecksPage.url);
-//         I.waitForVisible(databaseChecksPage.fields.dbCheckPanelSelector, 30);
-//         await databaseChecksPage.verifyDatabaseChecksPageOpened();
-//     }
-// );
+Scenario(
+    'PMM-T236 Verify user is able to hover Failed Checks values and see tooltip @not-pr-pipeline',
+    async (I, databaseChecksPage, pmmSettingsPage) => {
+        await pmmSettingsPage.enableSTT();
+        const row = 1;
+        I.amOnPage(databaseChecksPage.url);
+        await databaseChecksPage.verifyDatabaseChecksPageOpened();
+        databaseChecksPage.mouseOverInfoIcon(row);
+        await databaseChecksPage.compareTooltipValues(row);
+    }
+);
 
-// Scenario(
-//     'PMM-T233 Verify user can see Number of failed checks at Home Page and open PMM Database Checks page from it @not-pr-pipeline',
-//     async (I, homePage, databaseChecksPage) => {
-//         I.amOnPage(homePage.url);
-//         I.waitForVisible(homePage.fields.checksPanelSelector, 30);
-//         await homePage.waitForCheckResultsToAppearInPanel();
-//         I.doubleClick(homePage.fields.sttFailedChecksPanelSelector);
-//         await databaseChecksPage.verifyDatabaseChecksPageOpened();
-//     }
-// );
-
-// Scenario(
-//     'PMM-T236 Verify user is able to hover Failed Checks values and see tooltip @not-pr-pipeline',
-//     async (I, databaseChecksPage) => {
-//         const row = 1;
-//         I.amOnPage(databaseChecksPage.url);
-//         await databaseChecksPage.verifyDatabaseChecksPageOpened();
-//         databaseChecksPage.mouseOverInfoIcon(row);
-//         await databaseChecksPage.compareTooltipValues(row);
-//     }
-// );
-
-// Scenario(
-//     'PMM-T241 Verify user can see correct service name for failed checks @not-pr-pipeline',
-//     async (I, databaseChecksPage) => {
-//         I.amOnPage(databaseChecksPage.url);
-//         await databaseChecksPage.verifyDatabaseChecksPageOpened();
-//         await databaseChecksPage.verifyServiceNamesExistence();
-//     }
-// );
+Scenario(
+    'PMM-T241 Verify user can see correct service name for failed checks @not-pr-pipeline',
+    async (I, databaseChecksPage, pmmSettingsPage) => {
+        await pmmSettingsPage.enableSTT();
+        I.amOnPage(databaseChecksPage.url);
+        await databaseChecksPage.verifyDatabaseChecksPageOpened();
+        await databaseChecksPage.verifyServiceNamesExistence();
+    }
+);
